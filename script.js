ee1089bf41a4348113e8cd5d1c814a52636b6dbc
MattiaNatella <mattianatella@gmail.com>
2024-11-26 09:38:22 +0100

    end of exercise

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ const ageField = document.getElementById('ageField')
 const generate = document.getElementById('generate')
 const reset = document.getElementById('generate')
 
+//assegno variabili area IL TUO BIGLIETTO
+let tName = document.getElementById('ticketName')
+let tOffer = document.getElementById('ticketOffer')
+let tCarroz = document.getElementById('ticketCarroz')
+let tCpCode = document.getElementById('ticketCpCode')
+let tPrice = document.getElementById('ticketPrice')
+
 //assegno le variabili di logica
 const scontoMinorenni = 20;
 const scontoOver65 = 40;
 
-//LOGICA SCONTO
+//LOGICA SCONTO + COMPILAZIONE AREA IL TUO BIGLIETTO
 generate.addEventListener('click', () => {
 
   const prezzoBiglietto = kmField.value * 0.21;
@@ -27,6 +34,11 @@ generate.addEventListener('click', () => {
   //DISPLAY CARD IL TUO BIGLIETTO
   result.classList.toggle('d-none')
 
+  tName.innerHTML = name.value
+  tOffer.innerText = (ageField.value === 'minorenne') ? 'Sconto minorenne' : 'Prezzo intero'
+  tCarroz.innerText = Math.ceil(Math.random()*10);
+  tCpCode.innerText = '16TNRM'
+  tPrice.innerText = prezzoFinale.toFixed(2) + '€'
 
 })
 
