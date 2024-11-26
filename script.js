484dbc8d26c0cec66a309eed17e546b65d01977c
MattiaNatella <mattianatella@gmail.com>
2024-11-26 10:21:21 +0100

    true end

--- script.js
+++ script.js
@@ -18,35 +18,48 @@ const scontoMinorenni = 20;
 const scontoOver65 = 40;
 
 //LOGICA SCONTO + COMPILAZIONE AREA IL TUO BIGLIETTO
-generate.addEventListener('click', () => {
+generate.addEventListener('click', handlerClickGenerate)
 
+reset.addEventListener('click', handlerClickReset)
+
+
+
+
+//FUNCTIONS
+
+function getRandomNumber(min,max) {
+  return Math.floor(Math.random() * (max - min +1) + min)
+  }
+
+function handlerClickGenerate() {
+  
   const prezzoBiglietto = kmField.value * 0.21;
   let prezzoFinale = prezzoBiglietto 
   let message = 'Prezzo Intero'
-  if(ageField.value === 'minorenne'){
-    prezzoFinale *= 1 - scontoMinorenni / 100
-    message = 'Sconto Minorenne'
-   
-  }else if(ageField.value === 'over65'){
-      prezzoFinale = prezzoBiglietto * (1 - scontoOver65 / 100);
-      message = 'Sconto Over 65'
-  }
+
+    if(ageField.value === 'minorenne'){
+      prezzoFinale *= 1 - scontoMinorenni / 100
+      message = 'Sconto Minorenne'
+    
+    }else if(ageField.value === 'over65'){
+        prezzoFinale *= 1 - scontoOver65 / 100;
+        message = 'Sconto Over 65'
+    }
+
+    console.log(prezzoFinale)
+
+    result.classList.toggle('d-none')
+
+    tName.innerHTML = name.value
+    tOffer.innerText = message
+    tCarroz.innerText = getRandomNumber(1,12)
+    tCpCode.innerText = getRandomNumber(10000,1000000)
+    tPrice.innerText = prezzoFinale.toFixed(2) + '€'
+}
+
+function handlerClickReset() {
+
   
-  console.log(prezzoFinale)
-
-  //DISPLAY CARD IL TUO BIGLIETTO
-  result.classList.toggle('d-none')
-
-  tName.innerHTML = name.value
-  tOffer.innerText = message
-  tCarroz.innerText = Math.ceil(Math.random()*10);
-  tCpCode.innerText = getRandomNumber(10000,1000000)
-  tPrice.innerText = prezzoFinale.toFixed(2) + '€'
-
-})
-
-reset.addEventListener('click', () => {
- 
   result.classList.toggle('d-none')
 
   name.value = ''
@@ -58,11 +71,4 @@ reset.addEventListener('click', () => {
   tCarroz.innerText = ''
   tCpCode.innerText = ''
   tPrice.innerText = ''
-
-})
-
-//FUNCTIONS
-
-function getRandomNumber(min,max) {
-  return Math.floor(Math.random() * (max - min +1) - min)
-  }
+}
